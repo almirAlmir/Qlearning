86ce4fec4719349909dfd09b4f5a846d4610b28e
almirAlmir <aasc@cin.ufpe.br>
2023-07-27 21:22:37 -0300

    feat(client): Iniciando variaveis necessarias para o algoritmo

--- client.py
+++ client.py
@@ -1,1 +1,15 @@
 #Aqui vocês irão colocar seu algoritmo de aprendizado
+import numpy as np
+import connection as cn
+import random
+
+s = cn.connect(2037)
+act = ["left", "right", "jump"]                 #A ordem das colunas na Q-Table de ser
+                                                #Giro pra esquerda, Girot pra direita, Pulo pra frente
+Qtable = np.zeros(96, len(act))
+
+#Hyperparametros Alpha, Gamma e Epsilon. Optei por iniciar comm valores padrao abaixo
+
+alpha = 0.01
+gamma = 0.9
+epsilon = 0.1
