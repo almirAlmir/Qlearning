78bff3775ca247d47670ff1e0a7cedd8b3b7d3de
Vinícius Scala <viniciusscala@gmail.com>
2023-08-15 15:40:07 -0300

    make changes for presentation day

--- client.py
+++ client.py
@@ -7,11 +7,13 @@ act = ["left", "right", "jump"]     #A ordem das colunas na Q-Table de ser
                                     #Giro pra esquerda, Giro pra direita, Pulo pra frente
 Qtable = np.zeros((96, len(act)))
 
+print(Qtable)
+
 #Hyperparametros Alpha, Gamma e Epsilon. Optei por iniciar com valores padrao abaixo
 
-alpha = 0.01
-gamma = 0.9
-epsilon = 0.1
+alpha = 0.3
+gamma = 0.8
+epsilon = 0.4
 
 state = 0
 
@@ -29,9 +31,17 @@ def write_txt(q_tabel):
             file.write(text + "\n") #quebra linha
             
     file.close()
+
+def read_txt(filename):
+    with open(filename, "r") as file:
+        lines = file.readlines()
+        q_tabel = [list(map(int, line.strip().split())) for line in lines]
+    return q_tabel
     
 s = cn.connect(2037)
-for i in range (1, 1000):
+total_interactions = 1000
+for i in range (1, total_interactions):
+    print('rodada', i)
     
     finished = False
     
@@ -40,8 +50,10 @@ for i in range (1, 1000):
         action = 0
         
         if random.uniform(0, 1) < epsilon:
+            print('random epsilon', epsilon)
             action = random.randint(0, 2) #Uma das 3 açoes: left, right, jump
         else:
+            print('not random')
             value = -1000 #valor arbitrario
             for index in range (3): #percorre as acoes de 0 até 2
                     
@@ -51,17 +63,22 @@ for i in range (1, 1000):
                     
         current_state = state
         state, reward = cn.get_state_reward(s , act[action])
+        print('reward', reward)
         state = int(state, 2) #S2
+        print('state', state)
         
         #Equação de Bellman para atualização da Tabela_Q
         estimate = reward + gamma*np.max(Qtable[state]) #Estimativa de Q(s,a) pela Equação de Belman
         erro = estimate - Qtable[current_state][action] #Estimativa - Valor atual
         Qtable[current_state][action] += alpha * erro #nomes escolhidos de acordo com slides
-                      
+
         if reward == 300 or reward <= -100:
+            if reward == 300:
+                wins+=1
             finished = True
             break
     
-    wins+=1
+    epsilon -= (epsilon/total_interactions)
+    print('wins', wins)
     write_txt(Qtable)                
                     
